4a2a30b7faf5793c5a3dfa5005adb8fb9663a266
nikisalli <niki.salli@gmail.com>
2022-11-06 04:04:37 +0100

    fix

--- lib/nikfemm/src/constants.hpp
+++ lib/nikfemm/src/constants.hpp
@@ -1,11 +1,7 @@
 #ifndef NIK_CONSTANTS_H
 #define NIK_CONSTANTS_H
 
-#define float32_t float
-#define float64_t double
-
 #define EPSILON                     1e-9
-#define MACHINE_EPSILON             4.940656e-324
 #define PI                          3.14159265358979323846
 #define MU_0                        4 * PI * 1e-7
 
