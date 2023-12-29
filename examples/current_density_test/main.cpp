1d420eb95dfc81c123d48d1ba73b4fbb19f16a57
nikisalli <niki.salli@gmail.com>
2023-12-29 22:30:06 +0100

    Update CMake minimum version to 3.5.0

--- examples/current_density_test/main.cpp
+++ examples/current_density_test/main.cpp
@@ -5,16 +5,36 @@
 int main(int argc, char** argv) {
     nikfemm::CurrentDensitySimulation simulation;
     simulation.depth = 70e-6;
-    simulation.mesh.max_triangle_area = 1e-4;
+    simulation.mesh.max_triangle_area = 1e-0;
 
-    simulation.mesh.drawing.drawRectangle(nikfemm::Vector(0, 0), nikfemm::Vector(1, 1));
+    // simulation.mesh.drawing.drawRectangle(nikfemm::Vector(0, 0), nikfemm::Vector(1, 1));
+
+    // simulation.mesh.drawing.drawRegion(nikfemm::Vector(0.5, 0.5), {nikfemm::current_density_materials::copper});
+
+    simulation.mesh.drawing.drawPolygon(
+        {
+            nikfemm::Vector(0, 0),
+            nikfemm::Vector(4, 0),
+            nikfemm::Vector(4, 4),
+            nikfemm::Vector(0, 4),
+            nikfemm::Vector(0, 2),
+            nikfemm::Vector(1, 2),
+            nikfemm::Vector(1, 3),
+            nikfemm::Vector(3, 3),
+            nikfemm::Vector(3, 1),
+            nikfemm::Vector(1, 1),
+            nikfemm::Vector(1, 1.5),
+            nikfemm::Vector(0, 1.5)
+
+        }
+    );
 
     simulation.mesh.drawing.drawRegion(nikfemm::Vector(0.5, 0.5), {nikfemm::current_density_materials::copper});
 
     auto system = simulation.generateSystem();
 
     system.addDirichletBoundaryCondition(0, 0.0);
-    system.addDirichletBoundaryCondition(1, 1.0);
+    system.addDirichletBoundaryCondition(2, 1.0);
 
     simulation.solve(system);
 
